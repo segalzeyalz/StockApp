2d6cac8136ee1c2bf329011e8869ecc196155c99
segalzeyalz <segaleseo@gmail.com>
2019-01-10 22:14:27 +0200

    design text

--- src/Components/InfoComps/Title.jsx
+++ src/Components/InfoComps/Title.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CSS from './InfoComps.css';
 const Title = (props) => {
-  return <h1>{props.name}</h1>
+  return <h1 className={CSS.Title}>{props.name}</h1>
 };
 export default Title;
