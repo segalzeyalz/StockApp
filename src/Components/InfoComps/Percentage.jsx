215b9909fbe16676228f5459dc8887bd3f36af4a
segalzeyalz <segaleseo@gmail.com>
2019-01-12 11:18:56 +0200

    adding functionality to percents

--- src/Components/InfoComps/Percentage.jsx
+++ src/Components/InfoComps/Percentage.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import CSS from './InfoComps.css';
 const Precentage = (props) => {
-  return <h2>{props.absVal} ({props.percent}%)</h2>
+    let str = '';
+    let CSSClass;
+    if(props.absVal>=0){
+        str+= `+${props.absVal} (+${props.percent}%)`;
+        CSSClass = CSS.PositiveValue;
+    }else{
+        str+= `${props.absVal} (${props.percent}%)`;
+        CSSClass = CSS.NegativeValue;
+    }
+  return <h2 className={CSSClass}>{str}</h2>
 };
 export default Precentage;
